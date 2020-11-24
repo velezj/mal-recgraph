0d2632cccfa3b5f8c2f77c2acd537641e67b0a91
Javier Velez <velejester@gmail.com>
2020-11-24 09:39:48 -0500

    Makefile for running wget commands from link files

---
+++ novel-updates-fetch/Download.mk
@@ -0,0 +1,30 @@
+SHELL := /bin/bash
+WGET_WAIT = 10
+WGET_LIMIT_RATE = 900k
+
+
+all: $(addsuffix .touch,$(subst links_,finished_,$(wildcard links_*)))
+
+
+finished_%.touch: links_%
+	{ \
+	set -e ;\
+	set -x ;\
+	name=$(subst links_,,$<) ;\
+	dirname=chapters_$${name} ;\
+	urlsname=urls_$${name}.txt ;\
+	logfile=log.txt ;\
+	echo "-----------------------------------------------------" ;\
+	echo "Starting $${name}" ;\
+	echo "-----------------------------------------------------" ;\
+	mkdir -p "$${dirname}" ;\
+	cd "$${dirname}" ;\
+	cat ../$< | tr --delete '"' > "$${urlsname}" ;\
+	wget --wait=${WGET_WAIT} --random-wait --input-file="$${urlsname}" --verbose ;\
+	}
+	touch "${@}"
+
+
+links_%.get: finished_%.touch
+	echo "fetched"
+
