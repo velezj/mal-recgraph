4853464780d7755911d8edb7a498c4fb765af934
Javier Velez <velejester@gmail.com>
2021-07-05 18:04:57 -0400

    adding chapter titles

--- webnovel-fetch/make-merged/isekai-yururi-kikou/make-single-file.fish
+++ webnovel-fetch/make-merged/isekai-yururi-kikou/make-single-file.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
 set BASE_PATH "/home/velezj/projects/personal/repos/mal-recgraph/webnovel-fetch/isekai-yururi-kikou/novelonomicon.com/novels/isekai-yururi-kikou"
-set OUTPUT_FILE "all-merged.chunk"
+set OUTPUT_FILE "all-merged.chunk.html"
 
 rm -rf "$OUTPUT_FILE"
 for i in (seq 1 293)
-    if ./process-file.sh "$BASE_PATH/chapter-$i/index.html" >> "$OUTPUT_FILE"
+    if ./process-file.sh "$BASE_PATH/chapter-$i/index.html" "$i" >> "$OUTPUT_FILE"
 	echo "woot"
     else
 	exit 255
--- webnovel-fetch/make-merged/isekai-yururi-kikou/process-file.sh
+++ webnovel-fetch/make-merged/isekai-yururi-kikou/process-file.sh
@@ -5,6 +5,7 @@ set -u
 set -o pipefail
 
 INPUT="$1"
+CHAPTER_NUM="$2"
 SED_OUTPUT="${INPUT}_sed.html"
 TIDY_OUTPUT="${SED_OUTPUT}_tidy.xhtml"
 LINT_OUTPUT="${TIDY_OUTPUT}_xmllint.xhtml"
@@ -17,7 +18,7 @@ cp "$INPUT" "$SED_OUTPUT"
 # sed -i 's/<script>\(.*?\)<\/script>/<script\/>/g' "$SED_OUTPUT"
 sed -i '/<script/,/<\/script>/d' "$SED_OUTPUT"
 sed -i -n '/TOC<\/span>/,/TOC<\/span>/p' "$SED_OUTPUT"
-echo "<!DOCTYPE html><html><head></head><body><div class=\"entry-content\"><p>" > tmpfile && cat "$SED_OUTPUT" >> tmpfile && mv tmpfile "$SED_OUTPUT"
+echo "<!DOCTYPE html><html><head><title>Chapter $CHAPTER_NUM </title></head><body><div class=\"entry-content\"><h1 class=\"chapter\">Chapter $CHAPTER_NUM </h1><p>" > tmpfile && cat "$SED_OUTPUT" >> tmpfile && mv tmpfile "$SED_OUTPUT"
 #echo "</p></p></div></body></html>" >> "$SED_OUTPUT"
 tidy -xml -output "$TIDY_OUTPUT" "$SED_OUTPUT"
 xmllint --dropdtd --noblanks --nocatalogs --nocdata --nonet --nsclean --xmlout --format "$TIDY_OUTPUT" > "$LINT_OUTPUT"
